c897a1753333289e1544e6f518490d74e78ab8d1
Shimon <shimondj@gmail.com>
2023-01-13 22:24:10 +0200

    Initial commit

--- src/com/company/Attack.java
+++ src/com/company/Attack.java
@@ -1,69 +1,24 @@
 package com.company;
 
+import java.util.Random;
+
 public class Attack {
-    private String name;
     private int cost;
-    private int damage;
-    private boolean isAvailable;
-    private int level;
+    private int minDamage;
+    private int maxDamage;
 
-    public Attack(String name, int cost, int damage, int level) {
-        this.name = name;
+    public Attack(int cost, int minDamage, int maxDamage) {
         this.cost = cost;
-        this.damage = damage;
-        this.isAvailable = isAttackAvailable();
-        this.level = level;
+        this.minDamage = minDamage;
+        this.maxDamage = maxDamage;
     }
 
-    public static final Attack kick = new Attack("Kick",0,4,1);
-
-    public String getName() {
-        return name;
+    public int getCost(){
+        return this.cost;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public boolean isAttackAvailable() {
-        boolean isAvailable = false;
-        if (this.level == Pokemon.getCurrentLevel()) {
-            isAvailable = true;
-        }
-
-        return isAvailable;
-    }
-
-    public String toString() {
-        return
-                "Name :" + name + "\n" +
-                        "Cost :" + cost + "\n" +
-                        "Damage :" + damage + "\n" +
-                        "Is Available :" + isAvailable;
-
-    }
-
-    public int getCost() {
-        return cost;
-    }
-
-    public void setCost(int cost) {
-        this.cost = cost;
-    }
-
-    public int getDamage() {
-        return damage;
-    }
-
-    public void setDamage(int damage) {
-        this.damage = damage;
-    }
-
-    public boolean isAvailable() {
-        return isAvailable;
-    }
-
-    public void setAvailable(boolean available) {
-        isAvailable = available;
+    public int getDamage(){
+        Random random = new Random();
+        return random.nextInt(this.maxDamage - this.minDamage) + this.minDamage + 1;
     }
 }
--- src/com/company/Charmander.java
+++ src/com/company/Charmander.java
@@ -1,17 +1,47 @@
 package com.company;
 
-public class Charmander extends FirePokemon {
+import java.util.Scanner;
 
-    int scratchDamage = random.nextInt(Constants.SCRATCH_DAMAGE) + Constants.SCRATCH_DAMAGE_COMPLETION;
-    Attack[] charmanderAttacks = new Attack[1];
+public class Charmander extends Pokemon {
+    private Attack scratch;
+    private Attack flameTail;
+    private Attack fieryBlast;
 
-    {
-        new Attack("Scratch", 15, scratchDamage, 1);
+    public Charmander() {
+        super(80, 80, (int) (40 * 0.75), 40, 1);
+        this.scratch = new Attack(15, 25, 30);
+        this.flameTail = new Attack(40, 30, 50);
+        this.fieryBlast = new Attack(50, 50, 30);
+    }
+
+    public void attack(Pokemon enemy) {
+        this.setTurnCounter(this.getTurnCounter() + 1);
+        Scanner scanner = new Scanner(System.in);
+        Attack attack = null;
+        int userChoice;
+        do {
+            System.out.println("Choose an Attack" +
+                    "|1| - for Scratch" +
+                    "|2| - for Flame Tail" +
+                    "|3| - for Fiery Blast");
+            userChoice = scanner.nextInt();
+            switch (userChoice) {
+                case 1 -> attack = this.scratch;
+                case 2 -> attack = this.flameTail;
+                case 3 -> attack = this.fieryBlast;
+            }
+
+        } while (!isValidAttack(userChoice));
+        this.performAttack(enemy, attack);
+
+    }
+
+    public boolean isValidAttack(int attack) {
+        boolean valid = attack == Constants.LEVEL_ONE;
+        if ((attack == Constants.LEVEL_TWO) && (this.getLevel() >= Constants.LEVEL_TWO)) valid = true;
+        if ((attack == Constants.LEVEL_THREE) && (this.getLevel() == Constants.LEVEL_THREE)) valid = true;
+        return valid;
     }
 
 
-    public Charmander(int maxHp, int currentHp, int maxAttackPts, int currentAttackPts, String name, int currentLevel, int maxLevel, Attack[] attacks) {
-        super(80, currentHp, 40, currentAttackPts, "Charmander", 1, 3, attacks);
-
-    }
 }
