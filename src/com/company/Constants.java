9d5aec12f49b9791ee228cb9e0545aeea30faf41
Shimon <shimondj@gmail.com>
2023-01-13 22:22:47 +0200

    Initial commit

--- src/com/company/Constants.java
+++ src/com/company/Constants.java
@@ -1,24 +1,16 @@
 package com.company;
 
 public class Constants {
-    public static final int MIN_HP_TO_ADD = 5;
-    public static final int MAX_HP_TO_ADD = 30;
-    public static final int MIN_ATTACK_POINTS_TO_ADD = 0;
-    public static final int MAX_ATTACK_POINTS_TO_ADD = 40;
-    public static final int ATTACK_PTS_TO_EVOLVE_1 = 25;
-    public static final int HP_PTS_TO_EVOLVE_1 = 20;
-    public static final int ATTACK_PTS_TO_EVOLVE_2 = 40;
-    public static final int HP_PTS_TO_EVOLVE_2 = 30;
-    public static final int FIRE_POKEMON_SELF_DAMAGE = 10;
-    public static final int FIRE_POKEMON_SELF_DAMAGE_COMPLETION = 3;
-    public static final int QUARTER_CHANCE = 5;
-    public static final int NUMBER_TO_AVOID_SELF_DAMAGE = 4;
-    public static final int HP_INCREMENT_PER_TURN = 4;
-    public static final int ATTACK_PTS_INCREMENT_PER_TURN = 4;
-    public static final int PERCENT_REPRESENTATION = 100;
-    public static final int MIN_HP_TO_SELF_DISCHARGE = 20;
-    public static final int SCRATCH_DAMAGE = 5;
-    public static final int SCRATCH_DAMAGE_COMPLETION = 25;
+    public static final int MAX_LIFE_ADD = 30;
+    public static final int MIN_LIFE_ADD = 5;
+    public static final int SKIP_TURN_BOUND = 3;
+    public static final int CRITICAL_DAMAGE = 3;
+    public static final int LEVEL_ONE = 1;
+    public static final int LEVEL_TWO = 2;
+    public static final int LEVEL_THREE = 3;
+
+
+
 
 
 
